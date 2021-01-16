4308857dd8a4e4a9ec159b6417ef64234247262a
Gabriel Veloso <gabriel.azuirson@ufpe.br>
2021-01-16 16:08:33 -0300

    front-web initials files

---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import "./style.css";
+import {ReactComponent as Logo} from './logo.svg';
+
+
+function Navbar(){
+    return (
+        <nav className="main-navbar">
+            <Logo />
+
+        </nav>
+    )
+}
+
+export default Navbar;
